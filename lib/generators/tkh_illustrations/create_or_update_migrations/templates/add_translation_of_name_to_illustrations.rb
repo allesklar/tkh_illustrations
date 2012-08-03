a211a976ed4e369275b5b6b2a24a3d545e45ed91
Swami Atma <swami@ohlalaweb.com>
2012-08-03 16:13:53 +0200

    Name translations do migrate existing data.

--- lib/generators/tkh_illustrations/create_or_update_migrations/templates/add_translation_of_name_to_illustrations.rb
+++ lib/generators/tkh_illustrations/create_or_update_migrations/templates/add_translation_of_name_to_illustrations.rb
@@ -1,9 +1,13 @@
 class AddTranslationOfNameToIllustrations < ActiveRecord::Migration
   def self.up
-    Illustration.create_translation_table! name: :string
+    Illustration.create_translation_table!({
+          name: :string
+        }, {
+          :migrate_data => true
+        }) 
   end
   
   def self.down
-    Illustration.drop_translation_table!
+    Illustration.drop_translation_table! :migrate_data => true
   end
 end
