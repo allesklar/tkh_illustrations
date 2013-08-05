eca61a9b102e9573fab76d38ebf9f6d14e7ad347
Swami Atma <swami@ohlalaweb.com>
2013-08-05 19:40:27 +0200

    Added an alphabetical scope to illustration model.

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -4,18 +4,19 @@ Globalize::ActiveRecord::Translation.class_eval do
 end
 
 class Illustration < ActiveRecord::Base
-  
+
   attr_accessible :image, :name
-  
+
   validates_presence_of :name
-  
+
   translates :name
-  
+
   mount_uploader :image, ImageUploader
-  
+
   def to_param
     name ? "#{id}-#{name.to_url}" : id
   end
 
   scope :by_recent, :order => 'updated_at desc'
+  scope :alphabetically, order('name')
 end
