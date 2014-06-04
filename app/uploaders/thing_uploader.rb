602d2b351e391dac9f1a0a3846506ac751ba5aad
Swami Atma <swami@ohlalaweb.com>
2014-06-04 14:41:13 +0200

    Fixed path to thing uploader assets and added a link to the asset in the downloads index view.

--- app/uploaders/thing_uploader.rb
+++ app/uploaders/thing_uploader.rb
@@ -16,7 +16,7 @@ class ThingUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"  --- that was the suggested one
-    "system/downloads/#{model.class.to_s.underscore}/#{model.id}"
+    "system/things/#{model.class.to_s.underscore}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
